711f59fe476071ea2ef3f3db30a263187b399a23
vip6111111 <601115211@qq.com>
2019-01-16 15:59:43 +0800

    更新执行文件

--- php7/run.sh
+++ php7/run.sh
@@ -1,18 +1,22 @@
-@echo off
-
-Web=
-Docker=D:/docker/
-Php7=php7/etc
+#!/bin/bah
 
 
 
+nginx_web_dir="/var/www/html"
+docker_php_dir="/docker/php7/"
+docker_name="xwx/php-fpm:7.2.5"
 
-if exist %Docker%Php7 (
+if [ ! -d $nginx_web_dir ]; then
+	echo "mkdir -p $nginx_web_dir"
+	mkdir -p $nginx_web_dir	
+fi
 
-) else (
-    md  %Docker%Php7
-)
+if [ ! -d $docker_php_dir ]; then
+	echo "mkdir -p $docker_php_dir"
+	mkdir -p $docker_php_dir
+fi
 
-docker run -p 9000:9000 --name  php-fpm  -v D:/web:/var/www/html -v D:/docker/php7/etc:/usr/local/etc  -d xwx/php-fpm:7.2.5
+
+docker run -p 9000:9000 --name  php-fpm  -v ${nginx_web_dir}:/var/www/html -v ${docker_php_dir}/etc:/usr/local/etc  -d ${docker_name}
 
 
