687166b37b414fa643637b40901ed1459c9e84bf
vip601115211 <601115211@qq.com>
2019-06-07 09:15:01 +0800

    fix bug

--- php7/run.sh
+++ php7/run.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
 
 
 
-nginx_web_dir="/var/www/html"
-docker_php_dir="/docker/php7/"
+nginx_web_dir="${HOME}/var/www/html"
+docker_php_dir="${HOME}/docker/php7"
 docker_name="xwx/php-fpm:7.2.5"
 
 if [ ! -d $nginx_web_dir ]; then
@@ -16,7 +16,17 @@ if [ ! -d $docker_php_dir ]; then
 	mkdir -p $docker_php_dir
 fi
 
+cp -rf ./resources/php7/etc/* ${docker_php_dir}/etc/
 
-docker run -p 9000:9000 --name  php-fpm  -v ${nginx_web_dir}:/var/www/html -v ${docker_php_dir}/etc:/usr/local/etc  -d ${docker_name}
-
+old_docker_name=`docker ps -a|grep php-fpm|awk '{print $1}'`
+if [ -n "$old_docker_name" ]; then
+    rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
+    echo "删除之前安装php-fpm容器:${old_docker_name},${rm_res}"
+fi
+res=`docker run -p 9000:9000 --name  php-fpm  -v ${nginx_web_dir}:/var/www/html -v ${docker_php_dir}/etc:/usr/local/etc  -d ${docker_name}`
+if [ -n "$res" ]; then
+    echo "install success [$res]"
+else
+    echo "install failure"
+fi
 
