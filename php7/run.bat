7fb1c4d94b9f0adcf5bbe1353d555bb55eb813c6
vip6111111 <601115211@qq.com>
2019-07-16 08:32:37 +0800

    增加mcrypt 扩展

--- php7/run.bat
+++ php7/run.bat
@@ -22,4 +22,18 @@ if  exist %docker_php_dir% (
     md  %docker_php_dir%
 )
 
+
+cp -rf ./resources/php7/etc/* ${docker_php_dir}/etc/
+
+old_docker_name=`docker ps -a|findstr php-fpm|awk '{print $1}'`
+if [ -n "$old_docker_name" ]; then
+    rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
+    echo "删除之前安装php-fpm容器:${old_docker_name},${rm_res}"
+fi
 docker run -p 9000:9000 --name  php-fpm  -v %nginx_web_dir%:/var/www/html -v %docker_php_dir%etc:/usr/local/etc  -d %docker_name%
+#if [ -n "$res" ]; then
+#    echo "install success [$res]"
+#else
+#    echo "install failure"
+#fi
+
