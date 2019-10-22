1b5618add1b0432f656c5c6cf9b5f1844cbb82be
xiongweixing <xiongweixing@credithc.com>
2019-10-22 14:16:42 +0800

    更新

--- nginx/run.bat
+++ nginx/run.bat
@@ -1,10 +1,39 @@
 @echo off
 
-set nginx_web_dir="D:/web"
-set docker_nginx_dir="D:/docker/nginx/"
+set nginx_web_dir=D:/web
+set docker_nginx_dir=D:/docker/nginx/
 set docker_name="xwx/nginx:1.10"
 
+if  not exist %nginx_web_dir% (
+    md  %nginx_web_dir%
+)
+
+if not  exist %docker_nginx_dir% (
+    md  %docker_nginx_dir%
+)
+
+if  not exist %docker_nginx_dir%/conf (
+    md  %docker_nginx_dir%/conf
+)
+
+xcopy "./resources/conf" "%docker_nginx_dir%/conf"  /e /h /d /y
+set used=false
+for /F "usebackq tokens=1" %%i in (`"docker ps -a|find "nginx""`) do (
+    set old_docker_name=%%i
+    set used=true
+)
+
+ if "%used%"=="true" (
+	docker stop %old_docker_name%
+	docker rm %old_docker_name%
+)
 
 
 docker run --name nginx  --link php-fpm:php-fpm -p 80:80 -p443:443 -v %nginx_web_dir%:/var/www/html -v %docker_nginx_dir%conf/nginx.conf:/etc/nginx/nginx.conf -v  %docker_nginx_dir%conf/conf.d:/etc/nginx/conf.d -v %docker_nginx_dir%logs:/var/log/nginx -d %docker_name%
 
+if %ERRORLEVEL% == 0 (
+   echo install success
+) ELSE (
+   echo install failure
+)
+pause
--- nginx/run.sh
+++ nginx/run.sh
@@ -3,6 +3,7 @@
 nginx_web_dir="/var/www/html"
 docker_nginx_dir="/docker/nginx/"
 docker_name="xwx/nginx:1.10"
+docker_nginx_conf_dir=${nginx_home_dir}/conf
 
 if [ ! -d $nginx_web_dir ]; then
 	echo "mkdir -p $nginx_web_dir"
@@ -14,6 +15,21 @@ if [ ! -d $docker_nginx_dir ]; then
 	mkdir -p $docker_nginx_dir
 fi
 
+if [ !-d $docker_nginx_conf_dir ]; then
+	echo "mkdir -p $docker_nginx_conf_dir"
+	mkdir -p $docker_nginx_conf_dir
+fi
 
-docker run --name nginx  --link php-fpm:php-fpm -p 80:80 -p443:443 -v ${nginx_web_dir}:/var/www/html -v ${nginx_home_dir}/conf/nginx.conf:/etc/nginx/nginx.conf -v  ${nginx_home_dir}/conf/conf.d:/etc/nginx/conf.d -v ${nginx_home_dir}/logs:/var/log/nginx -d ${docker_name}
+cp -rf ./resources/conf/* ${docker_php_dir}/conf/
 
+old_docker_name=`docker ps -a|grep nginx|awk '{print $1}'`
+if [ -n "$old_docker_name" ]; then
+    rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
+    echo "删除之前安装php-fpm容器:${old_docker_name},${rm_res}"
+fi
+res=`docker run --name nginx  --link php-fpm:php-fpm -p 80:80 -p443:443 -v ${nginx_web_dir}:/var/www/html -v ${nginx_home_dir}/conf/nginx.conf:/etc/nginx/nginx.conf -v  ${nginx_home_dir}/conf/conf.d:/etc/nginx/conf.d -v ${nginx_home_dir}/logs:/var/log/nginx -d ${docker_name}`
+if [ -n "$res" ]; then
+    echo "install success [$res]"
+else
+    echo "install failure"
+fi
