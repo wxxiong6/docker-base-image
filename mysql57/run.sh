e542dd7ed4e8b15975067c34f855cf5a956facdb
wxxiong6 <wxxiong6@gmail.com>
2022-02-17 15:50:45 +0800

    更新

--- mysql57/run.sh
+++ mysql57/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+#set -x
 
-
-docker_mysql_dir="/docker/mysql"
+docker_mysql_dir=`pwd`
 docker_name="mysql:5.7"
 
 
@@ -17,7 +17,7 @@ fi
 old_docker_name=`docker ps -a|grep mysql|awk '{print $1}'`
 if [ -n "$old_docker_name" ]; then
     rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
-    echo "删除之前安装php-fpm容器:${old_docker_name},${rm_res}"
+    echo "删除之前安装容器:'${old_docker_name}',${rm_res}"
 fi
 
 res=`docker run -p 3306:3306 --name mysql -v ${docker_mysql_dir}/conf:/etc/mysql/conf.d -v ${docker_mysql_dir}/logs:/logs -v ${docker_mysql_dir}/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -d ${docker_name}`
