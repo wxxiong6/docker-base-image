1453c0a225d3ce495fd66520590b957979ce53bb
wxxiong6 <wxxiong6@gmail.com>
2022-06-22 15:13:31 +0800

    m

--- mysql80/run.sh
+++ mysql80/run.sh
@@ -11,13 +11,10 @@ if [ ! -d $docker_mysql_dir ]; then
 fi
 
 
-
-
-
 old_docker_name=`docker ps -a|grep mysql|awk '{print $1}'`
 if [ -n "$old_docker_name" ]; then
     rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
-    echo "删除之前安装容器:'${old_docker_name}',${rm_res}"
+    echo "delete old container: '${old_docker_name}', ${rm_res}"
 fi
 	   
 res=`docker run -p 3306:3306 --name mysql -v ${docker_mysql_dir}/conf:/etc/mysql/conf.d -v ${docker_mysql_dir}/logs:/logs -v ${docker_mysql_dir}/data:/var/lib/mysql -e TZ=Asia/Shanghai -e MYSQL_ROOT_PASSWORD=root -d ${docker_name} --default-authentication-plugin=mysql_native_password`    
