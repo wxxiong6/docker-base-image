93931e0ed2eb5d37dd720b453ac7c12b72598e08
wxxiong6 <wxxiong6@gmail.com>
2022-11-21 14:17:31 +0800

    feat: add mongdb

---
+++ mongdb/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+docker_mysql_dir=$(dirname $(dirname $(pwd)))/mysql80
+docker_name="mysql:8.0"
+
+
+if [ ! -d $docker_mysql_dir ]; then
+	echo "mkdir -p $docker_mysql_dir"
+	mkdir -p $docker_mysql_dir
+fi
+
+
+old_docker_name=`docker ps -a|grep mysql|awk '{print $1}'`
+if [ -n "$old_docker_name" ]; then
+    rm_res=`docker stop ${old_docker_name} && docker rm ${old_docker_name}`
+    echo "delete old container: '${old_docker_name}', ${rm_res}"
+fi
+	   
+res=`docker run -p 3306:3306 --name mysql -v ${docker_mysql_dir}/conf:/etc/mysql/conf.d -v ${docker_mysql_dir}/logs:/logs -v ${docker_mysql_dir}/data:/var/lib/mysql -e TZ=Asia/Shanghai -e MYSQL_ROOT_PASSWORD=root -d ${docker_name} --default-authentication-plugin=mysql_native_password`    
+
+if [ -n "$res" ]; then
+    echo "install success [$res]"
+else
+    echo "install failure"
+fi
